30065aa8b3ba1ba8820afe4d63a7c660cdbdb77d
ignacio <nacho@localhost.com>
2023-05-02 20:41:53 -0600

    tag helper para los campos

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -7,6 +7,7 @@ namespace ManejadorDePresupuestos.Models
         public string Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")] //esta anotacion sirve para que el campo no se envie vacio
         [StringLength(maximumLength:50, MinimumLength = 3, ErrorMessage = "La logitud del campo {0} debe estar entre {2} y {1}")]
+        [Display(Name = "Nombre del tipo cuenta")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
