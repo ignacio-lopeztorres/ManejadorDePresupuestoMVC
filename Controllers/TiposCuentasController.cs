c02c7c2fe6d21f82911b077883439861f84459c9
ignacio <nacho@localhost.com>
2023-05-02 19:50:34 -0600

    Creando controlador y vistas para Los tipos de cuentas.

---
+++ Controllers/TiposCuentasController.cs
@@ -0,0 +1,23 @@
+﻿using ManejadorDePresupuestos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejadorDePresupuestos.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        private readonly ILogger<TiposCuentasController> _logger;
+
+        public TiposCuentasController(ILogger<TiposCuentasController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Crear() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta) { 
+            return View();
+        }
+    }
+}
---
+++ Models/TipoCuenta.cs
@@ -0,0 +1,10 @@
+﻿namespace ManejadorDePresupuestos.Models
+{
+    public class TipoCuenta
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+    }
+}
