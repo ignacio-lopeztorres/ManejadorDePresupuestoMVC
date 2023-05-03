57df78fb41beebf9a5387e471ac05c893af918a6
Ignacio Lopez Torres <ignacio.lopeztorres@hotmail.com>
2023-05-02 23:24:08 -0600

    Validaciones personalizadas por atributo

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ManejadorDePresupuestos.Validaciones;
+using System.ComponentModel.DataAnnotations;
 
 namespace ManejadorDePresupuestos.Models
 {
@@ -6,21 +7,9 @@ namespace ManejadorDePresupuestos.Models
     {
         public string Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")] //esta anotacion sirve para que el campo no se envie vacio
-        [StringLength(maximumLength:50, MinimumLength = 3, ErrorMessage = "La logitud del campo {0} debe estar entre {2} y {1}")]
-        [Display(Name = "Nombre del tipo cuenta")]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
-
-        /*Pruebas de otras validaciones por defecto*/
-        [Required(ErrorMessage = "El campo {0} es requerido")]
-        [EmailAddress(ErrorMessage = "El Campo debe de ser un correo electrónico válido")]
-        public string Email { get; set; }
-        [Range(minimum:18, maximum: 120, ErrorMessage = "El valor debe estar entre {1} y {2}")]
-        public int Edad { get; set; }
-        [Url(ErrorMessage = "El campo debe de ser una URL válida")]
-        public string URL { get; set; }
-        [CreditCard(ErrorMessage = "La targeta de credito no es válida")]
-        public string CreditCard { get; set; }
     }
 }
---
+++ Validaciones/PrimeraLetraMayusculaAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejadorDePresupuestos.Validaciones
+{
+    public class PrimeraLetraMayusculaAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+            var primeraLetra = value.ToString()[0].ToString();
+            //valida que la primera letra sea mayuscula
+            if (primeraLetra != primeraLetra.ToUpper())
+            {
+                return new ValidationResult("La primera letra debe ser mayúscula");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
