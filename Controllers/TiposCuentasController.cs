e0b70e968236bb68cc6b4697688e4f10042980b7
Ignacio Lopez Torres <ignacio.lopeztorres@hotmail.com>
2023-05-03 00:41:21 -0600

    Creando la conexion con la base de datos usando dapper

--- Controllers/TiposCuentasController.cs
+++ Controllers/TiposCuentasController.cs
@@ -1,17 +1,27 @@
-﻿using ManejadorDePresupuestos.Models;
+﻿using Dapper;
+using ManejadorDePresupuestos.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace ManejadorDePresupuestos.Controllers
 {
     public class TiposCuentasController : Controller
     {
         private readonly ILogger<TiposCuentasController> _logger;
+        private readonly string connectionString;
 
-        public TiposCuentasController(ILogger<TiposCuentasController> logger)
+        public TiposCuentasController(ILogger<TiposCuentasController> logger,
+            IConfiguration configuration)
         {
             _logger = logger;
+            this.connectionString = configuration.GetConnectionString("DefaultConnection");
         }
         public IActionResult Crear() {
+
+            //uso de dapper
+            using (var connection = new SqlConnection(connectionString)) {
+                var query = connection.Query("SELECT 1").FirstOrDefault();
+            };
             return View();
         }
 
