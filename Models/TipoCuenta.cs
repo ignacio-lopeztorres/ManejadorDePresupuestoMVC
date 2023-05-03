5384686e2a62ecae1234489e20f9fdf273036b36
Ignacio Lopez Torres <ignacio.lopeztorres@hotmail.com>
2023-05-02 23:59:07 -0600

    Reestablece a la validacion por atributo

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -3,27 +3,27 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ManejadorDePresupuestos.Models
 {
-    public class TipoCuenta:IValidatableObject
+    public class TipoCuenta //:IValidatableObject
     {
         public string Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")] //esta anotacion sirve para que el campo no se envie vacio
-        //[PrimeraLetraMayuscula]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Nombre != null && Nombre.Length > 0)
-            {
-                var primeraLetra = Nombre[0].ToString();
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Nombre != null && Nombre.Length > 0)
+        //    {
+        //        var primeraLetra = Nombre[0].ToString();
 
-                if (primeraLetra != primeraLetra.ToUpper())
-                {
-                    yield return new ValidationResult("La primera letra debe ser mayúscula", new[] { nameof(Nombre)});
-                    //si no se agrega new[] { nameof(Nombre)} indica que la validacion es a nivel de modelo
-                }
-            }
-        }
+        //        if (primeraLetra != primeraLetra.ToUpper())
+        //        {
+        //            yield return new ValidationResult("La primera letra debe ser mayúscula", new[] { nameof(Nombre)});
+        //            //si no se agrega new[] { nameof(Nombre)} indica que la validacion es a nivel de modelo
+        //        }
+        //    }
+        //}
     }
 }
