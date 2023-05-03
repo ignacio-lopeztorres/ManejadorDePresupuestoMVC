2a1aea3d84998a8812f5df194d9277b9e92a6e4a
ignacio <nacho@localhost.com>
2023-05-02 20:30:08 -0600

    Visualizando los errores de validacion.

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -5,7 +5,8 @@ namespace ManejadorDePresupuestos.Models
     public class TipoCuenta
     {
         public string Id { get; set; }
-        [Required] //esta anotacion sirve para que el campo no se envie vacio
+        [Required(ErrorMessage = "El campo {0} es requerido")] //esta anotacion sirve para que el campo no se envie vacio
+        [StringLength(maximumLength:50, MinimumLength = 3, ErrorMessage = "La logitud del campo {0} debe estar entre {2} y {1}")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
