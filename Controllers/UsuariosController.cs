d35311fa390d891d3da7ad4e7580b2adb902bca9
Ignacio Lopez Torres <ignacio.lopeztorres@hotmail.com>
2023-05-16 16:13:38 -0600

    Agregando formulario de Registro

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,28 @@
+﻿using ManejadorDePresupuestos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejadorDePresupuestos.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Registro(RegistroViewModel modelo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(modelo);
+            }
+            return RedirectToAction("Index", "Transacciones");
+        }
+    }
+}
---
+++ Models/RegistroViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejadorDePresupuestos.Models
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
